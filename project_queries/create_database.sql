3c5f6be50549badc7ebb2cc41037c82a4286ee18
Matthew Ames <mga321@gmail.com>
2020-05-18 17:15:24 -0400

    Define tables for census database

---
+++ project_queries/create_database.sql
@@ -0,0 +1,28 @@
+-- Create database
+CREATE TABLE census_statistics (
+
+);
+
+
+
+CREATE TABLE states (
+
+);
+
+
+
+CREATE TABLE schools (
+
+);
+
+
+
+CREATE TABLE school_statistics (
+
+);
+
+
+
+CREATE TABLE schools_and_states (
+
+);
