397ee2272faf24e111c89e17eccc5f9fde4d3280
Matthew Ames <mga321@gmail.com>
2020-05-19 09:47:03 -0400

    Add data types to census_statistics table

--- project_queries/create_database.sql
+++ project_queries/create_database.sql
@@ -1,24 +1,24 @@
 -- Create database
 CREATE TABLE census_statistics (
-  zip_code,
-  state_code,
-  pop_total,
-  median_household_income,
-  pct_edu_hs,
-  pct_edu_somecollege_under1yr
-  pct_edu_somecollege_1plusyrs,
-  pct_edu_attain_assoc,
-  pct_edu_attain_bach,
-  pct_edu_attain_master,
-  pct_edu_attain_prof,
-  pct_edu_attain_doct
-  pct_white	pct_black,
-  pct_asian,
-  pct_hispanic,
-  pct_amerindian,
-  pct_nativehawaii,
-  pct_tworaces,
-  pct_otherrace
+  zip_code    CHAR(5),
+  state_code    CHAR(2),
+  pop_total   INT,
+  median_household_income   MONEY,
+  pct_edu_hs    DECIMAL(7, 4),
+  pct_edu_somecollege_under1yr    DECIMAL(7, 4),
+  pct_edu_somecollege_1plusyrs    DECIMAL(7, 4),
+  pct_edu_attain_assoc    DECIMAL(7, 4),
+  pct_edu_attain_bach    DECIMAL(7, 4),
+  pct_edu_attain_master    DECIMAL(7, 4),
+  pct_edu_attain_prof    DECIMAL(7, 4),
+  pct_edu_attain_doct    DECIMAL(7, 4),
+  pct_white	pct_black    DECIMAL(7, 4),
+  pct_asian    DECIMAL(7, 4),
+  pct_hispanic    DECIMAL(7, 4),
+  pct_amerindian    DECIMAL(7, 4),
+  pct_nativehawaii    DECIMAL(7, 4),
+  pct_tworaces    DECIMAL(7, 4),
+  pct_otherrace    DECIMAL(7, 4)
 );
 
 
