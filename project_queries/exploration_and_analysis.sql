5b8734f10f8a833476447422bbcc34e4bcb6c3d6
Matthew Ames <mga321@gmail.com>
2020-05-20 09:57:15 -0400

    Complete part 2 question 1 number of schools by locale

--- project_queries/exploration_and_analysis.sql
+++ project_queries/exploration_and_analysis.sql
@@ -3,7 +3,7 @@
 --Question 1:
 
 -- How many public high schools are in each zip code?
--- Query:
+-- Query 1:
 WITH individual_counts AS (
 	SELECT COUNT(*) AS count_of_schools
 	FROM schools
@@ -34,8 +34,8 @@ ORDER BY 1;
 
 -- Question 2:
 
--- How many high schools are in each state?
--- Query:
+-- How many public high schools are in each state?
+-- Query 1:
 SELECT st.state_name, COUNT(sc.*) num_of_schools
 FROM schools sc
 JOIN states st
@@ -58,4 +58,58 @@ Vermont has 45 schools
 Hawaii has 39 schools
 District of Columbia" has 28 schools
 Delaware has 28 schools
+*/
+
+
+
+
+
+-- Part 2:
+
+-- Question 1:
+
+-- How many public high schools are in each locale?
+-- Query 1:
+CREATE TABLE locales (
+	locale_code		CHAR(2)		PRIMARY KEY,
+	locale_type		CHAR(6),
+	locale_size		CHAR(7)
+);
+
+INSERT INTO locales
+VALUES (11, 'City', 'Large'),
+		(12, 'City', 'Midsize'),
+		(13, 'City',  'Small'),
+		(21, 'Suburb', 'Large'),
+		(22, 'Suburb', 'Midsize'),
+		(23, 'Suburb', 'Small'),
+		(31, 'Town', 'Fringe'),
+		(32, 'Town', 'Distant'),
+		(33, 'Town', 'Remote'),
+		(41, 'Rural', 'Fringe'),
+		(42, 'Rural', 'Distant'),
+		(43, 'Rural', 'Remote');
+
+-- Query 2:
+SELECT l.locale_type, l.locale_size, COUNT(school_id) num_of_schools
+FROM schools s
+JOIN locales l
+ON l.locale_code = s.locale_code
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+
+-- Answer:
+/*
+Suburb Large has 3554 schools
+Rural Distant	has 2349 schools
+City Large has 2024 schools
+Rural Fringe has 2004 schools
+Rura Remote has 1754 schools
+Town Distant has 1219 schools
+City Small has 879 schools
+Town Remote has 764 schools
+City Midsize has 745 schools
+Town Fringe has 549 schools
+Suburb Midsize has 464 schools
+Suburb Small has 311 schools
 */
