02b00113fb30ece102fe31a7296562e184e54c9b
Matthew Ames <mga321@gmail.com>
2020-05-20 20:56:04 -0400

    Complete part 3 question one for whether students score higher on math or reading

---
+++ project_queries/exploration_and_analysis_part_3.sql
@@ -0,0 +1,28 @@
+-- Part 3:
+
+--Question 1:
+
+-- On average, do students perform better on math or reading?
+-- Query 1:
+WITH top_subject AS (
+	SELECT CASE WHEN pct_proficient_math > pct_proficient_reading THEN 1
+				ELSE 0 END AS math_greater,
+			CASE WHEN pct_proficient_reading > pct_proficient_math THEN 1
+				ELSE 0 END AS reading_greater,
+			CASE WHEN pct_proficient_math = pct_proficient_reading THEN 1
+				ELSE 0 END AS tie_scores
+	FROM school_statistics
+	WHERE pct_proficient_math IS NOT NULL AND pct_proficient_reading IS NOT NULL
+)
+
+SELECT ROUND((1.0 * SUM(math_greater))/COUNT(*), 4) pct_schools_with_math_greater,
+		ROUND((1.0 * SUM(reading_greater))/COUNT(*), 4) pct_schools_with_reading_greater,
+		ROUND((1.0 * SUM(tie_scores))/COUNT(*), 4) pct_schools_with_tied_scores
+FROM top_subject;
+
+/*
+Answer:
+percent of schools with greater math scores = 18.23%
+percent of schools with greater reading scores = 67.75%
+percent of schools with tied scores = 14.01%
+*/
